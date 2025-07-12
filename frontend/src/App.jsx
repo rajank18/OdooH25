814cab1cb2ead4e8f1864e3d434a26b011939173
Darshan <155416385+Darshanh20@users.noreply.github.com>
2025-07-12 13:29:04 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,20 @@
 import './App.css';
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './components/Header';
 import Homepage from './pages/Homepage';
 import Loginpage from './pages/Loginpage';
-import ProfilePage from './pages/Userprofile';
+import ProfilePage from './pages/Userprofile'
 
 function AppWrapper() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
-  // Dummy user data
-  const [userData, setUserData] = useState({
-    name: 'Rajan Sharma',
-    image: 'https://ui-avatars.com/api/?name=Rajan+Sharma&background=0D8ABC&color=fff',
-    email: 'rajan.sharma@example.com',
-    location: 'Ahmedabad, India',
-    availability: 'WEEKENDS',
-    isPublic: true,
-  });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState({ name: '', image: '' });
 
   const handleLogin = (name) => {
     setIsLoggedIn(true);
     setUserData({
-      ...userData,
       name,
-      image: `https://ui-avatars.com/api/?name=${encodeURIComponent(name)}&background=0D8ABC&color=fff`,
+      image: `https://ui-avatars.com/api/?name=${encodeURIComponent(name)}&background=0D8ABC&color=fff`
     });
   };
 
